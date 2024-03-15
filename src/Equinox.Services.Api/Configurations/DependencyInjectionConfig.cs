35956bc0622d395c9c8a7f311c117b9dbce12c95
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:44 -0300

    comment

--- src/Equinox.Services.Api/Configurations/DependencyInjectionConfig.cs
+++ src/Equinox.Services.Api/Configurations/DependencyInjectionConfig.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using Equinox.Infra.CrossCutting.IoC;
-using Microsoft.Extensions.DependencyInjection;
+using System;
+using Equinox.Infra.CrossCutting.IoC; // This using statement includes the NativeInjectorBootStrapper class, which is used to register services in the DI container.
+using Microsoft.Extensions.DependencyInjection; // This using statement includes the IServiceCollection interface, which is used to configure services in the DI container.
 
 namespace Equinox.Services.Api.Configurations
 {
     public static class DependencyInjectionConfig
     {
+        /// <summary>
+        /// Adds the dependency injection configuration to the specified IServiceCollection. This includes registering services using the NativeInjectorBootStrapper class.
+        /// </summary>
+        /// <param name="services">The IServiceCollection to which the dependency injection configuration will be added.</param>
         public static void AddDependencyInjectionConfiguration(this IServiceCollection services)
         {
-            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (services == null) throw new ArgumentNullException(nameof(services)); // Validates that the services parameter is not null.
 
+            // Registers services using the NativeInjectorBootStrapper class.
             NativeInjectorBootStrapper.RegisterServices(services);
         }
     }
