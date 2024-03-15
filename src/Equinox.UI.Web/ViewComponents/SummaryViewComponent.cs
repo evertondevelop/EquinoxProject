3395a33e79129d0cb4084dd22f2a26eec87e6c94
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:19 -0300

    comment

--- src/Equinox.UI.Web/ViewComponents/SummaryViewComponent.cs
+++ src/Equinox.UI.Web/ViewComponents/SummaryViewComponent.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Equinox.UI.Web.ViewComponents
 {
+    /// <summary>
+    /// SummaryViewComponent is a class that represents a ViewComponent for rendering a summary view.
+    /// </summary>
     public class SummaryViewComponent : ViewComponent
     {
+        /// <summary>
+        /// Invoke is the method that is called when this ViewComponent is executed.
+        /// It returns a ViewResult that renders the Default view.
+        /// </summary>
+        /// <returns>A ViewResult that renders the Default view.</returns>
         public IViewComponentResult Invoke()
         {
             return View("Default");
