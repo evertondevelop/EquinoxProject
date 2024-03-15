e68dd585810c4d4fc69a6fcc2e7f3368bb3fe2a2
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:14 -0300

    comment

--- src/Equinox.Domain/Events/CustomerEventHandler.cs
+++ src/Equinox.Domain/Events/CustomerEventHandler.cs
@@ -1,31 +1,36 @@
-﻿using System.Threading;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 
 namespace Equinox.Domain.Events
 {
+    // This class handles various Customer events by implementing the INotificationHandler interface for each event type.
+    // It is responsible for performing any necessary actions when a Customer is registered, updated, or removed.
     public class CustomerEventHandler :
         INotificationHandler<CustomerRegisteredEvent>,
         INotificationHandler<CustomerUpdatedEvent>,
         INotificationHandler<CustomerRemovedEvent>
     {
+        // Handles the CustomerUpdatedEvent by sending a notification email.
         public Task Handle(CustomerUpdatedEvent message, CancellationToken cancellationToken)
         {
-            // Send some notification e-mail
+            // Send a notification email to the customer
 
             return Task.CompletedTask;
         }
 
+        // Handles the CustomerRegisteredEvent by sending a greetings email.
         public Task Handle(CustomerRegisteredEvent message, CancellationToken cancellationToken)
         {
-            // Send some greetings e-mail
+            // Send a greetings email to the customer
 
             return Task.CompletedTask;
         }
 
+        // Handles the CustomerRemovedEvent by sending a see you soon email.
         public Task Handle(CustomerRemovedEvent message, CancellationToken cancellationToken)
         {
-            // Send some see you soon e-mail
+            // Send a see you soon email to the customer
 
             return Task.CompletedTask;
         }
