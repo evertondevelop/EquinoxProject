30f2901108c85720d25ddcd0f756fb227d7408c1
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:33 -0300

    comment

--- src/Equinox.Domain/Models/Customer.cs
+++ src/Equinox.Domain/Models/Customer.cs
@@ -1,10 +1,20 @@
-﻿using System;
+using System;
 using NetDevPack.Domain;
 
 namespace Equinox.Domain.Models
 {
+    /// <summary>
+    /// Represents a customer in the Equinox system.
+    /// </summary>
     public class Customer : Entity, IAggregateRoot
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Customer"/> class.
+        /// </summary>
+        /// <param name="id">The unique identifier of the customer.</param>
+        /// <param name="name">The name of the customer.</param>
+        /// <param name="email">The email address of the customer.</param>
+        /// <param name="birthDate">The birth date of the customer.</param>
         public Customer(Guid id, string name, string email, DateTime birthDate)
         {
             Id = id;
@@ -13,13 +23,24 @@ namespace Equinox.Domain.Models
             BirthDate = birthDate;
         }
 
-        // Empty constructor for EF
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Customer"/> class without any initial values.
+        /// </summary>
         protected Customer() { }
 
+        /// <summary>
+        /// Gets the name of the customer.
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// Gets the email address of the customer.
+        /// </summary>
         public string Email { get; private set; }
 
+        /// <summary>
+        /// Gets the birth date of the customer.
+        /// </summary>
         public DateTime BirthDate { get; private set; }
     }
 }
