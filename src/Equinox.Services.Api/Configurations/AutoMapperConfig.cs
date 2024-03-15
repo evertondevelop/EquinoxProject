f0ae8662c657ad77156027569bc10be35344badc
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:47 -0300

    improve

--- src/Equinox.Services.Api/Configurations/AutoMapperConfig.cs
+++ src/Equinox.Services.Api/Configurations/AutoMapperConfig.cs
@@ -1,3 +1,14 @@
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class AutoMapperConfig
+{
+    public static void AddAutoMapperConfiguration(IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(AutoMapperConfig));
+    }
+}
+
 public void ConfigureServices(IServiceCollection services)
 {
     services.AddControllers();
