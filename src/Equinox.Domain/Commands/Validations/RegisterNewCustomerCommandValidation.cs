3d7ecc15a1ff7c80d99fc5d06eb669afea164def
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:13 -0300

    comment

--- src/Equinox.Domain/Commands/Validations/RegisterNewCustomerCommandValidation.cs
+++ src/Equinox.Domain/Commands/Validations/RegisterNewCustomerCommandValidation.cs
@@ -1,11 +1,22 @@
-﻿namespace Equinox.Domain.Commands.Validations
+namespace Equinox.Domain.Commands.Validations
 {
+    /// <summary>
+    /// Validation class for the RegisterNewCustomerCommand. Inherits from the CustomerValidation class.
+    /// </summary>
     public class RegisterNewCustomerCommandValidation : CustomerValidation<RegisterNewCustomerCommand>
     {
+        /// <summary>
+        /// Initializes a new instance of the RegisterNewCustomerCommandValidation class.
+        /// </summary>
         public RegisterNewCustomerCommandValidation()
         {
+            // Validate the name of the customer
             ValidateName();
+
+            // Validate the birth date of the customer
             ValidateBirthDate();
+
+            // Validate the email of the customer
             ValidateEmail();
         }
     }
