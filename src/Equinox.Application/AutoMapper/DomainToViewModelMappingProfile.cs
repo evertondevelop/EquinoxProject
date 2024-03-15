b8405990b99704b0700c0febff08a20260dbf8fd
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:17 -0300

    comment

--- src/Equinox.Application/AutoMapper/DomainToViewModelMappingProfile.cs
+++ src/Equinox.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -1,13 +1,20 @@
-﻿using AutoMapper;
+using AutoMapper;
 using Equinox.Application.ViewModels;
 using Equinox.Domain.Models;
 
 namespace Equinox.Application.AutoMapper
 {
+    /// <summary>
+    /// This class defines a mapping profile for converting domain models to view models.
+    /// </summary>
     public class DomainToViewModelMappingProfile : Profile
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DomainToViewModelMappingProfile"/> class.
+        /// </summary>
         public DomainToViewModelMappingProfile()
         {
+            // Creates a mapping from the Customer domain model to the CustomerViewModel view model.
             CreateMap<Customer, CustomerViewModel>();
         }
     }
