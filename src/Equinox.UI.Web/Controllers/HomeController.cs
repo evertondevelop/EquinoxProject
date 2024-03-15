6e25cca82f10af822823a63b0dc2fb4b4fc8c49c
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:51 -0300

    improve

--- src/Equinox.UI.Web/Controllers/HomeController.cs
+++ src/Equinox.UI.Web/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Equinox.UI.Web.Models;
+using Equinox.UI.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Equinox.UI.Web.Controllers
@@ -15,30 +15,29 @@ namespace Equinox.UI.Web.Controllers
         {
             var modelErro = new ErrorViewModel();
 
-            if (id == 500)
+            switch (id)
             {
-                modelErro.Message = "An error has occurred! Please try again later or contact our support.";
-                modelErro.Title = "An error has occurred!";
-                modelErro.ErrorCode = id;
-            }
-            else if (id == 404)
-            {
-                modelErro.Message = "The page you are looking for does not exist! <br /> If you have any questions please contact our support";
-                modelErro.Title = "Oops! Page not found.";
-                modelErro.ErrorCode = id;
-            }
-            else if (id == 403)
-            {
-                modelErro.Message = "You are not allowed to do this.";
-                modelErro.Title = "Access Denied";
-                modelErro.ErrorCode = id;
-            }
-            else
-            {
-                return StatusCode(500);
+                case 500:
+                    SetErrorDetails(modelErro, "An error has occurred! Please try again later or contact our support.", "An error has occurred!");
+                    break;
+                case 404:
+                    SetErrorDetails(modelErro, "The page you are looking for does not exist! <br /> If you have any questions please contact our support", "Oops! Page not found.");
+                    break;
+                case 403:
+                    SetErrorDetails(modelErro, "You are not allowed to do this.", "Access Denied");
+                    break;
+                default:
+                    return StatusCode(500);
             }
 
             return View("Error", modelErro);
+        }
+
+        private void SetErrorDetails(ErrorViewModel model, string message, string title)
+        {
+            model.Message = message;
+            model.Title = title;
+            model.ErrorCode = id;
         }
     }
 }
