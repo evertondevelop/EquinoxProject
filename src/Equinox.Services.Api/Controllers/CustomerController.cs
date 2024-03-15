01616b7bfa31a195afadccb953e697e389a39739
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:52:05 -0300

    improve

--- src/Equinox.Services.Api/Controllers/CustomerController.cs
+++ src/Equinox.Services.Api/Controllers/CustomerController.cs
@@ -1,70 +1,79 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Equinox.Application.EventSourcedNormalizers; // Namespace for event sourced normalizers
-using Equinox.Application.Interfaces; // Namespace for application interfaces
-using Equinox.Application.ViewModels; // Namespace for view models
-using Microsoft.AspNetCore.Authorization; // Namespace for authorization attributes
-using Microsoft.AspNetCore.Mvc; // Namespace for MVC attributes
-using NetDevPack.Identity.Authorization; // Namespace for custom authorization
+using Equinox.Application.EventSourcedNormalizers;
+using Equinox.Application.Interfaces;
+using Equinox.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetDevPack.Identity.Authorization;
 
-namespace Equinox.Services.Api.Controllers // Namespace for API controllers
+namespace Equinox.Services.Api.Controllers
 {
-    [Authorize] // Authorize all actions in this controller
-    public class CustomerController : ApiController // Base class for API controllers
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
     {
-        private readonly ICustomerAppService _customerAppService; // Customer app service instance
+        private readonly ICustomerAppService _customerAppService;
 
-        public CustomerController(ICustomerAppService customerAppService) // Constructor
+        public CustomerController(ICustomerAppService customerAppService)
         {
-            _customerAppService = customerAppService; // Initialize the customer app service
+            _customerAppService = customerAppService;
         }
 
-        [AllowAnonymous] // Allow anonymous access to this action
-        [HttpGet("customer-management")] // Map to GET /customer-management
-        public async Task<IEnumerable<CustomerViewModel>> Get() // Action for getting all customers
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CustomerViewModel>> GetById(Guid id)
         {
-            return await _customerAppService.GetAll(); // Return all customers from the app service
+            var customer = await _customerAppService.GetById(id);
+            return Ok(customer);
         }
 
-        [AllowAnonymous] // Allow anonymous access to this action
-        [HttpGet("customer-management/{id:guid}")] // Map to GET /customer-management/{id}
-        public async Task<CustomerViewModel> Get(Guid id) // Action for getting a customer by ID
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CustomerViewModel>>> GetAll()
         {
-            return await _customerAppService.GetById(id); // Return the customer with the given ID from the app service
+            var customers = await _customerAppService.GetAll();
+            return Ok(customers);
         }
 
-        [CustomAuthorize("Customers", "Write")] // Custom authorization attribute for write access to customers
-        [HttpPost("customer-management")] // Map to POST /customer-management
-        public async Task<IActionResult> Post([FromBody]CustomerViewModel customerViewModel) // Action for creating a new customer
+        [HttpPost]
+        public async Task<ActionResult<CustomerViewModel>> Post([FromBody] CustomerViewModel customerViewModel)
         {
-            if (!ModelState.IsValid) // If the model state is not valid
+            if (!ModelState.IsValid)
             {
-                return CustomResponse(ModelState); // Return a custom response with the model state errors
+                return BadRequest(ModelState);
             }
 
-            return CustomResponse(await _customerAppService.Register(customerViewModel)); // Otherwise, register the new customer and return the result
+            var customer = await _customerAppService.Register(customerViewModel);
+            return CreatedAtAction(nameof(GetById), new { id = customer.Id }, customer);
         }
 
-        [CustomAuthorize("Customers", "Write")] // Custom authorization attribute for write access to customers
-        [HttpPut("customer-management")] // Map to PUT /customer-management
-        public async Task<IActionResult> Put([FromBody]CustomerViewModel customerViewModel) // Action for updating a customer
+        [HttpPut]
+        public async Task<ActionResult<CustomerViewModel>> Put([FromBody] CustomerViewModel customerViewModel)
         {
-            if (!ModelState.IsValid) // If the model state is not valid
+            if (!ModelState.IsValid)
             {
-                return CustomResponse(ModelState); // Return a custom response with the model state errors
+                return BadRequest(ModelState);
             }
 
-            return CustomResponse(await _customerAppService.Update(customerViewModel)); // Otherwise, update the customer and return the result
+            var customer = await _customerAppService.Update(customerViewModel);
+            return Ok(customer);
         }
 
-        [CustomAuthorize("Customers", "Remove")] // Custom authorization attribute for remove access to customers
-        [HttpDelete("customer-management")] // Map to DELETE /customer-management
-        public async Task<IActionResult> Delete(Guid id) // Action for deleting a customer
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
         {
-            return CustomResponse(await _customerAppService.Remove(id)); // Delete the customer with the given ID and return the result
+            await _customerAppService.Remove(id);
+            return NoContent();
         }
 
-        [AllowAnonymous] // Allow anonymous access to this action
-        [HttpGet("customer-management/history/{id:guid}")] // Map to GET /customer-management/history/{id}
-        public async Task<IList<CustomerHistoryData>> History(Guid id) // Action for getting the
+        [HttpGet("{id}/history")]
+        public async Task<ActionResult<IEnumerable<CustomerHistoryData>>> GetHistory(Guid id)
+        {
+            var history = await _customerAppService.GetCustomerHistory(id);
+            return Ok(history);
+        }
+    }
+}
