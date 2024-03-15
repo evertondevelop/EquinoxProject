84aaba3ec323c95fb3fd7bff3637f6f65b9fd49f
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:48 -0300

    comment

--- src/Equinox.Application/Services/CustomerAppService.cs
+++ src/Equinox.Application/Services/CustomerAppService.cs
@@ -1,72 +1,72 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using AutoMapper;
+using AutoMapper; // AutoMapper is used for mapping objects between different layers of the application
 using Equinox.Application.EventSourcedNormalizers;
 using Equinox.Application.Interfaces;
 using Equinox.Application.ViewModels;
 using Equinox.Domain.Commands;
 using Equinox.Domain.Interfaces;
 using Equinox.Infra.Data.Repository.EventSourcing;
-using FluentValidation.Results;
-using NetDevPack.Mediator;
+using FluentValidation.Results; // FluentValidation is used for validation of objects
+using NetDevPack.Mediator; // NetDevPack.Mediator is used for mediating commands and queries
 
 namespace Equinox.Application.Services
 {
-    public class CustomerAppService : ICustomerAppService
+    public class CustomerAppService : ICustomerAppService // Implements the ICustomerAppService interface
     {
-        private readonly IMapper _mapper;
-        private readonly ICustomerRepository _customerRepository;
-        private readonly IEventStoreRepository _eventStoreRepository;
-        private readonly IMediatorHandler _mediator;
+        private readonly IMapper _mapper; // IMapper for mapping objects
+        private readonly ICustomerRepository _customerRepository; // Repository for managing customer data
+        private readonly IEventStoreRepository _eventStoreRepository; // Repository for managing event sourcing data
+        private readonly IMediatorHandler _mediator; // IMediator for mediating commands and queries
 
         public CustomerAppService(IMapper mapper,
                                   ICustomerRepository customerRepository,
                                   IMediatorHandler mediator,
-                                  IEventStoreRepository eventStoreRepository)
+                                  IEventStoreRepository eventStoreRepository) // Constructor
         {
-            _mapper = mapper;
-            _customerRepository = customerRepository;
-            _mediator = mediator;
-            _eventStoreRepository = eventStoreRepository;
+            _mapper = mapper; // Initialize IMapper
+            _customerRepository = customerRepository; // Initialize ICustomerRepository
+            _mediator = mediator; // Initialize IMediator
+            _eventStoreRepository = eventStoreRepository; // Initialize IEventStoreRepository
         }
 
-        public async Task<IEnumerable<CustomerViewModel>> GetAll()
+        public async Task<IEnumerable<CustomerViewModel>> GetAll() // Gets all customers
         {
-            return _mapper.Map<IEnumerable<CustomerViewModel>>(await _customerRepository.GetAll());
+            return _mapper.Map<IEnumerable<CustomerViewModel>>(await _customerRepository.GetAll()); // Maps the result to CustomerViewModel using IMapper
         }
 
-        public async Task<CustomerViewModel> GetById(Guid id)
+        public async Task<CustomerViewModel> GetById(Guid id) // Gets a customer by ID
         {
-            return _mapper.Map<CustomerViewModel>(await _customerRepository.GetById(id));
+            return _mapper.Map<CustomerViewModel>(await _customerRepository.GetById(id)); // Maps the result to CustomerViewModel using IMapper
         }
 
-        public async Task<ValidationResult> Register(CustomerViewModel customerViewModel)
+        public async Task<ValidationResult> Register(CustomerViewModel customerViewModel) // Registers a new customer
         {
-            var registerCommand = _mapper.Map<RegisterNewCustomerCommand>(customerViewModel);
-            return await _mediator.SendCommand(registerCommand);
+            var registerCommand = _mapper.Map<RegisterNewCustomerCommand>(customerViewModel); // Maps the CustomerViewModel to RegisterNewCustomerCommand using IMapper
+            return await _mediator.SendCommand(registerCommand); // Sends the command to the mediator
         }
 
-        public async Task<ValidationResult> Update(CustomerViewModel customerViewModel)
+        public async Task<ValidationResult> Update(CustomerViewModel customerViewModel) // Updates an existing customer
         {
-            var updateCommand = _mapper.Map<UpdateCustomerCommand>(customerViewModel);
-            return await _mediator.SendCommand(updateCommand);
+            var updateCommand = _mapper.Map<UpdateCustomerCommand>(customerViewModel); // Maps the CustomerViewModel to UpdateCustomerCommand using IMapper
+            return await _mediator.SendCommand(updateCommand); // Sends the command to the mediator
         }
 
-        public async Task<ValidationResult> Remove(Guid id)
+        public async Task<ValidationResult> Remove(Guid id) // Removes a customer
         {
-            var removeCommand = new RemoveCustomerCommand(id);
-            return await _mediator.SendCommand(removeCommand);
+            var removeCommand = new RemoveCustomerCommand(id); // Creates a new RemoveCustomerCommand
+            return await _mediator.SendCommand(removeCommand); // Sends the command to the mediator
         }
 
-        public async Task<IList<CustomerHistoryData>> GetAllHistory(Guid id)
+        public async Task<IList<CustomerHistoryData>> GetAllHistory(Guid id) // Gets the history of a customer
         {
-            return CustomerHistory.ToJavaScriptCustomerHistory(await _eventStoreRepository.All(id));
+            return CustomerHistory.ToJavaScriptCustomerHistory(await _eventStoreRepository.All(id)); // Converts the history to CustomerHistoryData and returns it
         }
 
-        public void Dispose()
+        public void Dispose() // Disposes of the object
         {
-            GC.SuppressFinalize(this);
+            GC.SuppressFinalize(this); // Suppresses finalization of the object
         }
     }
 }
