7ab93a293d11ee9e1bd855dd7fa91213c0ec0eba
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:13 -0300

    comment

--- src/Equinox.Domain/Commands/CustomerCommand.cs
+++ src/Equinox.Domain/Commands/CustomerCommand.cs
@@ -1,16 +1,32 @@
-﻿using System;
+using System;
 using NetDevPack.Messaging;
 
 namespace Equinox.Domain.Commands
 {
+    // This is the base class for all customer-related commands in the system.
+    // It inherits from the Command class and provides properties for common customer attributes.
     public abstract class CustomerCommand : Command
     {
+        // The unique identifier for the customer.
         public Guid Id { get; protected set; }
 
+        // The name of the customer.
         public string Name { get; protected set; }
 
+        // The email address of the customer.
         public string Email { get; protected set; }
 
+        // The date of birth of the customer.
         public DateTime BirthDate { get; protected set; }
+
+        // Constructor for the CustomerCommand class.
+        // Sets the Id, Name, Email, and BirthDate properties.
+        protected CustomerCommand(Guid id, string name, string email, DateTime birthDate)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
     }
 }
