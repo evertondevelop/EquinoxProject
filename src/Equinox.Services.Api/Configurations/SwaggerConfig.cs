15316c193fea1f5dc947e3951be22647e9009bf5
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:25 -0300

    comment

--- src/Equinox.Services.Api/Configurations/SwaggerConfig.cs
+++ src/Equinox.Services.Api/Configurations/SwaggerConfig.cs
@@ -1,18 +1,26 @@
-﻿using System;
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
 namespace Equinox.Services.Api.Configurations
 {
+    /// <summary>
+    /// This class contains extension methods for configuring Swagger for the Equinox API.
+    /// </summary>
     public static class SwaggerConfig
     {
+        /// <summary>
+        /// Adds Swagger services to the specified IServiceCollection.
+        /// </summary>
+        /// <param name="services">The IServiceCollection to add Swagger services to.</param>
         public static void AddSwaggerConfiguration(this IServiceCollection services)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
 
             services.AddSwaggerGen(s =>
             {
+                // Configures the Swagger document for the Equinox API.
                 s.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Version = "v1",
@@ -22,6 +30,7 @@ namespace Equinox.Services.Api.Configurations
                     License = new OpenApiLicense { Name = "MIT", Url = new Uri("https://github.com/EduardoPires/EquinoxProject/blob/master/LICENSE") }
                 });
 
+                // Configures the security definition for the Swagger document.
                 s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
                     Description = "Input the JWT like: Bearer {your token}",
@@ -32,6 +41,7 @@ namespace Equinox.Services.Api.Configurations
                     Type = SecuritySchemeType.ApiKey
                 });
 
+                // Configures the security requirement for the Swagger document.
                 s.AddSecurityRequirement(new OpenApiSecurityRequirement
                 {
                     {
@@ -46,15 +56,21 @@ namespace Equinox.Services.Api.Configurations
                         new string[] {}
                     }
                 });
-
             });
         }
 
+        /// <summary>
+        /// Configures Swagger middleware for the specified IApplicationBuilder.
+        /// </summary>
+        /// <param name="app">The IApplicationBuilder to configure Swagger middleware for.</param>
         public static void UseSwaggerSetup(this IApplicationBuilder app)
         {
             if (app == null) throw new ArgumentNullException(nameof(app));
 
+            // Enables Swagger middleware to serve Swagger documents.
             app.UseSwagger();
+
+            // Configures Swagger UI middleware to display Swagger documents.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
