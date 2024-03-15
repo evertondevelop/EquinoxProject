4cc41efdfa5a80567cb75241e5cf05f28f648793
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:15 -0300

    comment

--- src/Equinox.Domain.Core/Models/ValueObject.cs
+++ src/Equinox.Domain.Core/Models/ValueObject.cs
@@ -1,36 +1,48 @@
-﻿namespace Equinox.Domain.Core.Models
+namespace Equinox.Domain.Core.Models
 {
+    /// <summary>
+    /// Abstract base class for Value Objects that need to implement equality comparisons.
+    /// </summary>
+    /// <typeparam name="T">The type of the derived Value Object.</typeparam>
     public abstract class ValueObject<T> where T : ValueObject<T>
     {
+        /// <summary>
+        /// Determines whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns>True if the current object is equal to the obj parameter; otherwise, false.</returns>
         public override bool Equals(object obj)
         {
+            /// Check if the object is of type T and assign it to a variable of type T.
             var valueObject = obj as T;
+            /// Call the protected abstract method EqualsCore to perform the comparison.
             return EqualsCore(valueObject);
         }
 
+        /// <summary>
+        /// Performs the actual equality comparison for the derived Value Object.
+        /// </summary>
+        /// <param name="other">The other Value Object to compare with the current object.</param>
+        /// <returns>True if the current object is equal to the other parameter; otherwise, false.</returns>
         protected abstract bool EqualsCore(T other);
 
+        /// <summary>
+        /// Serves as a hash function for a particular type.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
         public override int GetHashCode()
         {
+            /// Call the protected abstract method GetHashCodeCore to generate the hash code.
             return GetHashCodeCore();
         }
 
+        /// <summary>
+        /// Generates the hash code for the derived Value Object.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
         protected abstract int GetHashCodeCore();
 
-        public static bool operator ==(ValueObject<T> a, ValueObject<T> b)
-        {
-            if (a is null && b is null)
-                return true;
-
-            if (a is null || b is null)
-                return false;
-
-            return a.Equals(b);
-        }
-
-        public static bool operator !=(ValueObject<T> a, ValueObject<T> b)
-        {
-            return !(a == b);
-        }
-    }
-}
+        /// <summary>
+        /// Equality operator for Value Objects.
+        /// </summary>
+       
