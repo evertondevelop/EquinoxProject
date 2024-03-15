43de4337110323d5d7359201988bcb1d33982b98
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:50 -0300

    improve

--- src/Equinox.UI.Web/Configurations/IdentityConfig.cs
+++ src/Equinox.UI.Web/Configurations/IdentityConfig.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,21 +6,30 @@ namespace Equinox.UI.Web.Configurations
 {
     public static class IdentityConfig
     {
-
+        /// <summary>
+        /// Adds social authentication configuration to the specified IServiceCollection.
+        /// </summary>
+        /// <param name="services">The IServiceCollection to add the configuration to.</param>
+        /// <param name="configuration">The IConfiguration to read configuration values from.</param>
         public static void AddSocialAuthenticationConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
 
             services.AddAuthentication()
+                // Add Facebook authentication
                 .AddFacebook(o =>
                 {
-                    o.AppId = configuration["Authentication:Facebook:AppId"];
-                    o.AppSecret = configuration["Authentication:Facebook:AppSecret"];
+                    // Configure Facebook authentication with app ID and secret
+                    o.AppId = configuration.GetValue<string>("Authentication:Facebook:AppId");
+                    o.AppSecret = configuration.GetValue<string>("Authentication:Facebook:AppSecret");
                 })
+                // Add Google authentication
                 .AddGoogle(googleOptions =>
                 {
-                    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
-                    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+                    // Configure Google authentication with client ID and secret
+                    googleOptions.ClientId = configuration.GetValue<string>("Authentication:Google:ClientId");
+                    googleOptions.ClientSecret = configuration.GetValue<string>("Authentication:Google:ClientSecret");
                 });
         }
     }
