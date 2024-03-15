f9e99ca2050bc79ca13e78509e1a24dcecdd7312
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:47 -0300

    improve

--- src/Equinox.UI.Web/Models/ErrorViewModel.cs
+++ src/Equinox.UI.Web/Models/ErrorViewModel.cs
@@ -1,23 +1,4 @@
 namespace Equinox.UI.Web.Models
 {
     /// <summary>
-    /// Represents a view model for displaying error information to the user.
-    /// </summary>
-    public class ErrorViewModel
-    {
-        /// <summary>
-        /// Gets or sets the error code associated with this error.
-        /// </summary>
-        public int ErrorCode { get; set; }
 
-        /// <summary>
-        /// Gets or sets the title of the error message.
-        /// </summary>
-        public string Title { get; set; }
-
-        /// <summary>
-        /// Gets or sets the detailed message describing the error.
-        /// </summary>
-        public string Message { get; set; }
-    }
-}
