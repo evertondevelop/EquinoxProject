52c7bc9f0dc06636a198f1ab72722e9f22201c98
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:46 -0300

    improve

--- src/Equinox.Application/Interfaces/ICustomerAppService.cs
+++ src/Equinox.Application/Interfaces/ICustomerAppService.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Equinox.Application.EventSourcedNormalizers;
 using Equinox.Application.ViewModels;
 using FluentValidation.Results;
+using Equinox.Domain.Interfaces;
+using Equinox.Infrastructure.CrossCutting.Notification;
 
 namespace Equinox.Application.Interfaces
 {
@@ -43,4 +46,33 @@ namespace Equinox.Application.Interfaces
         /// Removes a customer from the system.
         /// </summary>
         /// <param name="id">The unique identifier of the customer to remove.</param>
-        /// <returns>A task
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task Remove(Guid id);
+
+        /// <summary>
+        /// Adds a customer to the system.
+        /// </summary>
+        /// <param name="customerViewModel">The customer view model to add.</param>
+        /// <returns>A task that returns a validation result object indicating whether the addition was successful or not.</returns>
+        Task<ValidationResult> Add(CustomerViewModel customerViewModel);
+
+        /// <summary>
+        /// Validates a customer view model.
+        /// </summary>
+        /// <param name="customerViewModel">The customer view model to validate.</param>
+        /// <returns>A task that returns a validation result object indicating whether the view model is valid or not.</returns>
+        Task<ValidationResult> Validate(CustomerViewModel customerViewModel);
+
+        /// <summary>
+        /// Publishes an event to the system.
+        /// </summary>
+        /// <param name="domainEvent">The domain event to publish.</param>
+        void PublishEvent(IDomainEvent domainEvent);
+
+        /// <summary>
+        /// Gets the notification messages.
+        /// </summary>
+        /// <returns>A list of notification messages.</returns>
+        List<NotificationMessage> GetNotificationMessages();
+
+        ///
