570c50cfd24862b1de5078d1d24b03d98c3a02ce
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:16 -0300

    comment

--- src/Equinox.UI.Web/Areas/Identity/IdentityHostingStartup.cs
+++ src/Equinox.UI.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting;  // Using the Microsoft.AspNetCore.Hosting namespace
 
 [assembly: HostingStartup(typeof(Equinox.UI.Web.Areas.Identity.IdentityHostingStartup))]
+// The HostingStartup attribute identifies this class as the startup class for the Identity area of the Equinox.UI.Web application
+
 namespace Equinox.UI.Web.Areas.Identity
 {
-    public class IdentityHostingStartup : IHostingStartup
+    public class IdentityHostingStartup : IHostingStartup  // Implementing the IHostingStartup interface
     {
-        public void Configure(IWebHostBuilder builder)
+        public void Configure(IWebHostBuilder builder)  // Configuring the web host builder
         {
-            builder.ConfigureServices((context, services) => {
+            builder.ConfigureServices((context, services) =>  // Configuring the services for the web host builder
+            {
+                // Add any necessary services for the Identity area here
             });
         }
     }
