805edb8e015772e72c1c557fdf282b37705fc78c
Everton_dev <62966401+evertondevelop@users.noreply.github.com>
2024-03-15 12:51:50 -0300

    improve

--- src/Equinox.Infra.Data/Migrations/EventStoreSQL/20161213130520_Initial.cs
+++ src/Equinox.Infra.Data/Migrations/EventStoreSQL/20161213130520_Initial.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Equinox.Infra.Data.Migrations.EventStoreSQL
@@ -8,26 +8,26 @@ namespace Equinox.Infra.Data.Migrations.EventStoreSQL
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "StoredEvent",
+                name: "StoredEvents",
                 columns: table => new
                 {
-                    Id = table.Column<Guid>(nullable: false),
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "NEWID()"),
                     AggregateId = table.Column<Guid>(nullable: false),
                     Data = table.Column<string>(nullable: true),
                     Action = table.Column<string>(type: "varchar(100)", nullable: true),
-                    CreationDate = table.Column<DateTime>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false, defaultValueSql: "GETUTCDATE()"),
                     User = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_StoredEvent", x => x.Id);
+                    table.PrimaryKey("PK_StoredEvents", x => x.Id);
                 });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "StoredEvent");
+                name: "StoredEvents");
         }
     }
 }
